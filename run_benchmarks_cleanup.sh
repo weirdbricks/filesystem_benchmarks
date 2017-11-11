762032639e8e0eb8ffb036eefad97c684dfcc969
Lampros <weirdbricks@users.noreply.github.com>
2017-11-10 21:56:47 -0800

    Create run_benchmarks_cleanup.sh

---
+++ run_benchmarks_cleanup.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+function mysql-test()
+{
+	benchmark=mysql
+	# set the MySQL directories
+	systemctl stop mysqld
+	mkdir -pv /media/mysql
+	chown -Rv mysql:mysql /media/mysql
+	echo "Starting MySQL..."
+	systemctl start mysqld
+	echo "Starting MySQL Slap..."
+	time mysqlslap --concurrency=50 --iterations=10 --number-char-cols=20 \
+	--number-int-cols=7 --auto-generate-sql --number-of-queries=2000 -v | tee /root/$filesystem-$benchmark.txt
+	echo "Stopping MySQL..."
+	systemctl stop mysqld
+}
+
+function mongo-test()
+{
+        benchmark=mongo
+	systemctl stop mongod
+	echo "preparing Mongo..."
+	mkdir -pv /media/mongo
+	chown -Rv mongod:mongod /media/mongo
+	benchmark=mongo
+	systemctl start mongod
+	mongo ycsb --eval "db.dropDatabase()"
+	cd /tmp/ycsb-0.12.0
+	./bin/ycsb load mongodb -s -P workloads/workloada -p recordcount=100000 -threads `nproc`
+	./bin/ycsb run mongodb-async -s -P workloads/workloada \
+	-p operationcount=100000 -threads `nproc` | egrep -i "runtime|throughput|return" | tee /root/$filesystem-$benchmark.txt
+	systemctl stop mongod
+}
+
+function fio-test()
+{
+	benchmark=fio
+	fio --randrepeat=1 --ioengine=libaio --gtod_reduce=1 --name=test --bs=4k --iodepth=64 --size=2G --readwrite=randrw --rwmixread=75 --filename /media/fiotest | grep iops | tee /root/$filesystem-$benchmark.txt
+}
+
+function mongo-perf-test()
+{
+	cd /tmp/mongo-perf-r20171009
+	time python benchrun.py -f testcases/simple_* -t `nproc` | tee /root/$filesystem-$benchmark.txt
+}
+
+# get the volume id
+volume=`find /dev/mapper/v* | head -n1`
+echo "---------------- Preparing EXT4 ----------------"
+
+# format the volume with ext4
+filesystem=ext4
+mkfs.ext4 $volume -F
+
+# mount the volume with ext4
+mount $volume /media -v
+
+echo "---------------- Running EXT4 tests ----------------"
+mysql-test
+mongo-test
+fio-test
+mongo-perf-test
+echo "bogooooooooon"
+exit 1
+
+echo "---------------- Preparing XFS tests ----------------"
+cd /
+umount /media
+filesystem=xfs
+mkfs.xfs $volume -f
+mount $volume /media -v
+
+echo "---------------- Running XFS tests ----------------"
+#mysql-test
+#mongo-test
+
+echo "---------------- Preparing JFS tests ----------------"
+cd /
+umount /media
+filesystem=jfs
+mkfs.jfs $volume -f
+mount $volume /media -v
+
+echo "---------------- Running JFS tests ----------------"
+#mysql-test
+#mongo-test
